4b7ce19638768ef58b2f16a0df6b20b2b0c76d91
jeeheaG <jeehea99@naver.com>
2024-05-09 17:54:24 +0900

    Comment : fix typo

--- programmers/implementation/L3_광고삽입_prefix_sum.py
+++ programmers/implementation/L3_광고삽입_prefix_sum.py
@@ -18,7 +18,7 @@ def solution(play_time, adv_time, logs):
     full_sec = str_to_sec(play_time)
     adv_sec = str_to_sec(adv_time)
     
-    #누적합 구하는 부분 다시짜기
+    #누적합 구하기
     #+- 배열
     view = [0]*(full_sec+1)
     for log_str in logs :
