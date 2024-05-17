a9ca765c98276fad211ff3749521d98255691e8f
jeeheaG <jeehea99@naver.com>
2024-05-17 23:40:57 +0900

    Comment : add

--- baekjoon/binary_search/Sil4_1789_수들의_합_binary_search_2.py
+++ baekjoon/binary_search/Sil4_1789_수들의_합_binary_search_2.py
@@ -1,5 +1,6 @@
 # v3 : 이진탐색..의 느낌을 더 살려서! (44ms)
 # TS : 이분탐색 시 다음 범위는 현재 중간값을 포함하지 않는 범위로 설정해줄 것!
+# TIP : 갱신하면서 탐색 : 정확한 정답인지 조건을 확인하지 않아도, 갱신하면서 이진탐색을 다 돌고 나면 최종적으로 정답만 남게 되어있음
 
 S = int(input())
 answer = 0
@@ -17,7 +18,7 @@ while start <= end :
         break
     elif add < S :
         start = mid + 1
-        answer = mid
+        answer = mid #TIP : 갱신하면서 탐색
 
     
 print(answer)
