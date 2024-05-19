7c6a78819e8bfd425eed365841b46345168708ed
jeeheaG <jeehea99@naver.com>
2024-05-20 02:37:55 +0900

    Solve : 2805 tree_cut

--- baekjoon/binary_search/Sil2_2805_나무자르기.py
+++ baekjoon/binary_search/Sil2_2805_나무자르기.py
@@ -1,13 +1,13 @@
 # v1 : 이진탐색
 # 그냥 클래식한 이진탐색으로　구현하면　될　것　같은데？
+# TS 1 : 클래식해보여도 알고리즘을 외워서 쓰려고 하지 말고 문제에 맞게 말랑하게 생각하거라 . . .
+# TS 2 : 이진탐색 시 양쪽 인덱스 같은 경우 포함해야 함!
 
 import sys
 input = sys.stdin.readline
 
 N, total = map(int, input().split())
 tree_arr = list(map(int, input().split()))
-
-tree_arr.sort()
 
 def cut(limit) :
 	get = 0
@@ -20,20 +20,19 @@ answer = 0
 
 #이진탐색
 left, right = 0, max(tree_arr)
-while left < right :
+while left <= right : #TS 2
 	mid = (right + left) // 2
 	get = cut(mid)
-	print(mid, get)
+	# print(mid, get)
 
+	#적어도 total만큼의 나무를 가져가야 하므로 total <= get 일 경우를 answer에 저장해둬야 함
 	if get < total :
-		print("get <= total")
-		answer = mid
-		left = mid + 1
+		right = mid - 1 #TS 1
 	elif get == total :
-		print("get <= total")
 		answer = mid
 		break
 	elif total < get :
-		right = mid - 1
+		answer = mid
+		left = mid + 1 #TS 1
 
 print(answer)
