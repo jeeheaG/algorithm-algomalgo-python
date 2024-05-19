11033ce75eb0708916243393c5036470116ed04c
jeeheaG <jeehea99@naver.com>
2024-05-20 02:54:30 +0900

    Comment : add

--- baekjoon/binary_search/Sil2_2805_나무자르기.py
+++ baekjoon/binary_search/Sil2_2805_나무자르기.py
@@ -1,7 +1,7 @@
 # v1 : 이진탐색
 # 그냥 클래식한 이진탐색으로　구현하면　될　것　같은데？
 # TS 1 : 클래식해보여도 알고리즘을 외워서 쓰려고 하지 말고 문제에 맞게 말랑하게 생각하거라 . . .
-# TS 2 : 이진탐색 시 양쪽 인덱스 같은 경우 포함해야 함!
+# TS 2 : 이진탐색 시 양쪽 인덱스가 같은 경우도 포함해 탐색해야 함!
 
 import sys
 input = sys.stdin.readline
@@ -9,6 +9,7 @@ input = sys.stdin.readline
 N, total = map(int, input().split())
 tree_arr = list(map(int, input().split()))
 
+#주어진 높이로 자른 나무 양을 계산하는 메서드
 def cut(limit) :
 	get = 0
 	for tree in tree_arr :
