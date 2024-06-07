4fa79031356f62155fbc77f9d6a7f8e3ffb0e334
jeeheaG <jeehea99@naver.com>
2024-06-07 23:54:03 +0900

    Solve : 1991 tree_traversal

---
+++ baekjoon/tree/Sil1_1991_트리_순회.py
@@ -0,0 +1,72 @@
+# v1 : 트리 (dfs)
+
+'''
+[문제 해석]
+- 주어진 트리를 전위, 중위, 후위 순회한 순으로 각각 출력
+- 항상 A가 루트노드임
+- 시간제한 : 2초
+
+순회 방식
+- 전위 순회 : 루트 → 왼쪽 → 오른쪽 자식 순으로 방문
+- 중위 순회 : 왼쪽 → 루트 → 오른쪽
+- 후위 순회 : 왼쪽 → 오른쪽 → 루트
+
+구상
+일단 트리를 입력받고.. 루트가 누군지 알려주니까 거기서부터 찾아가자
+세가지 순회 모두 현재 노드 기준으로 우선순위 먼저인 것부터 재귀호출 돌게 하고, 루트노드를 탐색하는 순서에 현재노드를 답에 추가해주면 된다.
+'''
+
+import sys
+input = sys.stdin.readline
+
+N = int(input()) #노드 개수
+tree = dict()
+for _ in range(N) :
+    root, l, r = input().split()
+    tree[root] = (l, r)
+    
+start = 'A'
+none = '.'
+result = []
+
+
+# 전위 순회
+def preorder_dfs(cur) :
+    global result
+    left, right = tree[cur]
+    result.append(cur)
+    if left != none :
+        preorder_dfs(left)
+    if right != none :
+        preorder_dfs(right)
+
+preorder_dfs(start)
+result.append('\n')
+
+# 중위 순회
+def inorder_dfs(cur) :
+    global result
+    left, right = tree[cur]
+    if left != none :
+        inorder_dfs(left)
+    result.append(cur)
+    if right != none :
+        inorder_dfs(right)
+
+inorder_dfs(start)
+result.append('\n')
+
+# 후위 순회
+def postorder_dfs(cur) :
+    global result
+    left, right =tree[cur]
+    if left != none :
+        postorder_dfs(left)
+    if right != none :
+        postorder_dfs(right)
+    result.append(cur)
+
+postorder_dfs(start)
+
+#출력
+print(''.join(result))
