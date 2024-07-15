0c715e3880e5bf4ff71883f60d0b617acd1d8acf
jeeheaG <jeehea99@naver.com>
2024-07-15 21:47:45 +0900

    Comment : add

--- baekjoon/_etc/class_2/11650 좌표정렬하기 Sil5.py
+++ baekjoon/_etc/class_2/11650 좌표정렬하기 Sil5.py
@@ -9,6 +9,6 @@ nums = [tuple(map(int, input().split())) for _ in range(N)]
 
 result = []
 for num in sorted(nums) :
-    result.append('%d %d'%(num))
+    result.append('%d %d'%(num)) # 문자열로 변환
 
 print('\n'.join(result))
