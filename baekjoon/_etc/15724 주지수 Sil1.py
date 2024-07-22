b38fc4dd2490891a647630c9a2d02a04063100ba
jeeheaG <jeehea99@naver.com>
2024-07-22 19:11:13 +0900

    Pause : 15724 1% wrong...

--- baekjoon/_etc/15724 주지수 Sil1.py
+++ baekjoon/_etc/15724 주지수 Sil1.py
@@ -1,49 +1,73 @@
-# v1 : 누적합
-#pypy로만 통과한 코드
+# v1 : dp(누적합)
+# TS : 테케 잘 통과하는데 0%도 안가고 틀렸습니다
+#       -> 솔루션보니까 논리는 맞는데.. board위에서 계산하지 말고 새로 dp테이블을 행, 열 하나씩 크게 0넣고 조건문 없이 짰음
+#       -> 왜 이렇게 짜도 1% 틀렸습니다???
 
 '''
 [문제해석]
-보드 입력받아서
-요구하는 값 출력해라?? 포인트가 뭐지
--> 그냥 구현했더니 시간초과 남. 뭘로 줄이는 거지?
-
-1 1 3 2 (1,1)위치부터 크기 3*2
-
-보드를 그냥 사람수가 아니라
-왼쪽부터 더해놓은 값으로 보드를 만들어놓음(누적합)
-합 구할 때 행별로 부분합 구하는 계산만 함
-
-행별로 말고 사각형으로 해야 python
+부분계산들이 중복됨
+dp로 누적합을 계산해두고
+요구하는 값 계산해 반환
 
 - 시간제한 : 2초
+- dp테이블 점화식 : dp[i][j] = dp[i][j] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+- 특정 구역합 계산할 때도 비슷한 방식
 '''
 
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
+
 board = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0]*(M+1) for _ in range(N+1)]
+
+#dp테이블 - TS : 이것도...
+for i in range(1, N+1) :
+    for j in range(1, M+1) :
+        dp[i][j] = board[i-1][j-1] + dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1]
+
+# print(*dp)
 
 K = int(input())
 coms = [list(map(int, input().split())) for _ in range(K)]
 
-#행별 누적합으로 변경
-for line in board :
-    for j in range(1, M) :
-        line[j] = line[j] + line[j-1]
-
 ans = []
 for com in coms :
-    for i in range(4) :
-        com[i] -= 1
-    x, y, x_size, y_size = com
-    
-    # print(com)
-    res = 0
-    for line in board[x:x_size+1] :
-        res += line[y_size] if 0 == y else line[y_size] - line[y-1]
-    
-    # print(res)
-    ans.append(res)
+    x1, y1, x2, y2 = com
+    ans.append(dp[x2][y2] - dp[x1][y1-1] - dp[x1-1][y1] + dp[x1-1][y1-1])
 
 print('\n'.join(map(str, ans)))
+
+# dp테이블 -TS : 0없이 board에 그대로 짰는데 뭔가 범위 계산이 잘못된 듯..0넣고 다시짤래
+# for i in range(N) :
+#     for j in range(M) :
+#         new = board[i][j]
+#         if 0 < j :
+#             new += board[i][j-1]
+#         if 0 < i :
+#             new += board[i-1][j]
+#         if 0 < i and 0 < j :
+#             new -= board[i-1][j-1]
+        
+#         board[i][j] = new
+        
+
+# K = int(input())
+# coms = [list(map(int, input().split())) for _ in range(K)]
+
+# ans = []
+# for com in coms :
+#     x1, y1, x2, y2 = com
+#     res = board[x2-1][y2-1]
+#     if 1 < x1 :
+#         res -= board[x1-1][y1]
+#     if 1 < y1 :
+#         res -= board[x1][y1-1]
+#     if 1 < x1 and 1 < y1 :
+#         res += board[x1-1][y1-1]
+    
+#     ans.append(res)
+
+# print('\n'.join(map(str, ans)))
